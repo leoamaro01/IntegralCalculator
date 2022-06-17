f6c0a0d020c82ff87ed41a447ee6b2969ee6c4d6
leoamaro01 <leoamaro2001@gmail.com>
2022-06-17 14:46:40 -0400

    Initial Commit

---
+++ cnsle/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using lib;
+using System;
+using System.Threading;
+namespace cnsle;
+
+public static class Program
+{
+    static string functionName = "e^x * sinx";
+    static decimal Function(decimal input)
+    {
+        // Insert function here
+        return (decimal)(Math.Exp((double)input) * Math.Sin((double)input));
+    }
+    static void Main(string[] args)
+    {
+        IntegralCalculator.optimalDivisionsPerUnit = 10000;
+
+        decimal lower = decimal.Parse(Request("Limite inferior:"));
+        decimal higher = decimal.Parse(Request("Limite superior:"));
+
+        Console.Write($"La integral definida de la funcion {functionName} entre {lower} y {higher} es: ");
+
+        Stopwatch watch = new();
+        watch.Start();
+
+        decimal integral = IntegralCalculator.Calculate(lower,
+                            higher,
+                            IntegralCalculator.GetOptimalPrecission(higher - lower),
+                            Function);
+        watch.Stop();
+
+        Console.WriteLine($"{integral} ~ {Math.Round(integral, 3)}");
+        Console.WriteLine($"Calculado en {watch.ElapsedMilliseconds}ms");
+    }
+    static string Request(string query)
+    {
+        Console.WriteLine(query);
+        return Console.ReadLine() ?? "";
+    }
+}
---
+++ lib/IntegralCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lib;
+public class IntegralCalculator
+{
+    public static decimal midpoint = 0.5m;
+    public static int optimalDivisionsPerUnit = 100;
+
+    public static int GetOptimalPrecission(decimal intervalLength)
+    => (int)Math.Ceiling(intervalLength * optimalDivisionsPerUnit);
+
+    public static decimal Calculate(
+        decimal lowerLimit,
+        decimal higherLimit,
+        int precission,
+        Func<decimal, decimal> function)
+    {
+        decimal delta = (higherLimit - lowerLimit) / precission;
+        decimal sum = 0;
+
+        for (int i = 0; i < precission; i++)
+            sum += Math.Abs(function(lowerLimit + (delta * i) + (delta * midpoint))) * delta;
+
+        return sum;
+    }
+}
