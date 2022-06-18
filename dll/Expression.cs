8864563b9a1362d40afd1b9ccad4973e1c73bb1d
alfrecampione <80720402+alfrecampione@users.noreply.github.com>
2022-06-18 01:10:44 -0400

    Refactoring and Expression class added

---
+++ dll/Expression.cs
@@ -0,0 +1,75 @@
+using System;
+namespace lib;
+
+public abstract class Expression
+{
+    public abstract decimal Evaluate();
+}
+public abstract class BinaryExpression : Expression
+{
+    readonly Expression left;
+    readonly Expression right;
+
+    public BinaryExpression(Expression left, Expression right)
+    {
+        this.left = left;
+        this.right = right;
+    }
+    public override decimal Evaluate()
+    {
+        return Evaluate(left.Evaluate(), right.Evaluate());
+    }
+    protected abstract decimal Evaluate(decimal left, decimal right);
+}
+public abstract class UnaryExpression : Expression
+{
+    readonly Expression expression;
+    public UnaryExpression(Expression e)
+    {
+        this.expression = e;
+    }
+    public override decimal Evaluate()
+    {
+        return Evaluate(expression.Evaluate());
+    }
+    protected abstract decimal Evaluate(decimal value);
+}
+public class BinaryGeneric : BinaryExpression
+{
+    Func<decimal, decimal, decimal> func;
+    public BinaryGeneric(Expression left, Expression right, Func<decimal, decimal, decimal> func) : base(left, right)
+    {
+        this.func = func;
+    }
+
+    protected override decimal Evaluate(decimal left, decimal right)
+    {
+        return func(left, right);
+    }
+}
+public class UnaryGeneric : UnaryExpression
+{
+    Func<decimal, decimal> func;
+    public UnaryGeneric(Expression e, Func<decimal, decimal> func) : base(e)
+    {
+        this.func = func;
+    }
+
+    protected override decimal Evaluate(decimal value)
+    {
+        return this.func(value);
+    }
+}
+public class Const : Expression
+{
+    readonly decimal value;
+    public Const(decimal value)
+    {
+        this.value = value;
+    }
+
+    public override decimal Evaluate()
+    {
+        return value;
+    }
+}
---
+++ program/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using lib;
+using System;
+using System.Threading;
+namespace cnsle;
+
+public static class Program
+{
+    static string functionName = "e^x * sinx";
+    static decimal Function(decimal input)
+    {
+        return (decimal)(SetExpression(input).Evaluate());
+        // Insert function here
+        //return (decimal)(Math.Exp((double)input) * Math.Sin((double)input));
+    }
+    static void Main(string[] args)
+    {
+        IntegralCalculator.optimalDivisionsPerUnit = 10000;
+
+        decimal lower = decimal.Parse(Request("Limite inferior:"));
+        decimal higher = decimal.Parse(Request("Limite superior:"));
+
+        Console.Write($"La integral definida de la funcion {functionName} entre {lower} y {higher} es: ");
+
+        Stopwatch watch = new();
+        watch.Start();
+
+        decimal integral = IntegralCalculator.Calculate(lower,
+                            higher,
+                            IntegralCalculator.GetOptimalPrecission(higher - lower),
+                            Function);
+        watch.Stop();
+
+        Console.WriteLine($"{integral} ~ {Math.Round(integral, 3)}");
+        Console.WriteLine($"Calculado en {watch.ElapsedMilliseconds}ms");
+    }
+    static string Request(string query)
+    {
+        Console.WriteLine(query);
+        return Console.ReadLine() ?? "";
+    }
+    static Expression SetExpression(decimal input)
+    {
+        Expression e = new BinaryGeneric(
+            new UnaryGeneric(
+                new Const((decimal)input),
+                ((x) => (decimal)Math.Exp((double)x))
+            ),
+            new UnaryGeneric(
+                new Const(input),
+                ((x) => (decimal)Math.Sin((double)x))
+            ),
+            ((x, y) => x * y)
+        );
+        return e;
+    }
+}
